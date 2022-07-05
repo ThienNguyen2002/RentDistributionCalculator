ef0b3e6b7bd4f9b3bc1c88d426916e42d1f0d728
Thien Nguyen <nguthien2002@gmail.com>
2022-07-04 23:10:16 -0600

    inital commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RentDistributionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the rent
+            Console.WriteLine("Enter the rent: ");
+            //Ask how many people are paying rent
+            Console.WriteLine("Enter the number of people paying rent: ");
+            //Ask each person's monthly salary
+            Console.WriteLine("Enter each person's monthly salary: ");
+            //If the combined salary of all people is lesser greater than the rent, then continue to do the calculations.
+            // Otherwise, people cannot afford the rent.
+
+            //Show's the percentage and show's how much each person needs to pay
+            Console.WriteLine("The percentage of each person's monthly salary is: ");
+        }
+    }
+}
